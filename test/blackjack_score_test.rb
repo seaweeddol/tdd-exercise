432d2e65d089aeecf3feb9dadbf2f7fa7d3b6d95
Jessica Stone <jstone4815@gmail.com>
2020-02-12 14:29:55 -0800

    add more tests to blackjack_score_test.rb

--- test/blackjack_score_test.rb
+++ test/blackjack_score_test.rb
@@ -32,22 +32,23 @@ describe 'Blackjack Score' do
 
   it 'calculates aces as 11 where it does not go over 21' do
     score = blackjack_score([2, 3, 'Ace'])
-
     expect(score).must_equal 16
   end
 
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
     score = blackjack_score([10, 3, 'Ace'])
-
     expect(score).must_equal 14
   end
 
   it 'raises an ArgumentError for invalid cards' do
     expect{blackjack_score(['banana', 54, 5])}.must_raise ArgumentError
+    expect{blackjack_score([54, 2, 5, 'King'])}.must_raise ArgumentError
   end
 
   it 'raises an ArgumentError for scores over 21' do
     expect{blackjack_score(['King', 6, 8, 3])}.must_raise ArgumentError
+    expect{blackjack_score([10, 8, 'Ace', 'Queen'])}.must_raise ArgumentError
+    expect{blackjack_score([3, 'Ace', 'Jack'])}.must_raise ArgumentError
   end
 
   it 'raises an ArgumentError for hands larger than 5' do
