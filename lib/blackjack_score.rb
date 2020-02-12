e38ca9240daa850206696613229db224f29a190e
Jessica Stone <jstone4815@gmail.com>
2020-02-12 14:22:52 -0800

    refactor blackjack_score.rb

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -11,34 +11,23 @@ VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 # - Must raise an `ArgumentError` if the hand contains more than five cards
 
 def blackjack_score(hand)
-  if hand.length > 5
-    raise ArgumentError
-  end
+  raise ArgumentError if hand.length > 5
 
   score = 0
 
   hand.each do |card|
-    if VALID_CARDS.include?(card)
-      if card == 'Jack' || card == 'Queen' || card == 'King'
-        score += 10
-      elsif card == 'Ace'
-        if (score + 11) >= 21
-          score += 1
-        else
-          score += 11
-        end
-      else
-        score += card
-      end
+    raise ArgumentError unless VALID_CARDS.include?(card)
+
+    if card == 'Jack' || card == 'Queen' || card == 'King'
+      score += 10
+    elsif card == 'Ace'
+      (score + 11) >= 21 ? score += 1 : score += 11
     else
-      raise ArgumentError
+      score += card
     end
 
-    if score > 21
-      raise ArgumentError
-    elsif score == 21
-      return "You won Blackjack!"
-    end
+    raise ArgumentError if score > 21
+    return "You won Blackjack!" if score == 21
   end
 
   return score
